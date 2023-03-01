60e2de4473f92b8944b2d5e9d5465d6d05365092
Ryan Adolf <ryanadolf123@gmail.com>
2023-03-01 01:52:13 -0800

    Favicon

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,8 +1,7 @@
 <script>
-    import Code from 'svelte-material-icons/CodeTags.svelte'
-    import Keyboard from 'svelte-material-icons/KeyboardOutline.svelte'
-    import Love from 'svelte-material-icons/HeartOutline.svelte'
-
+ import Code from 'svelte-material-icons/CodeTags.svelte'
+ import Keyboard from 'svelte-material-icons/KeyboardOutline.svelte'
+ import Love from 'svelte-material-icons/HeartOutline.svelte'
 </script>
 
 <p class="mb-2">
@@ -12,7 +11,7 @@
     Special thanks to everyone who has made the Dactyl keyboard possible, including <a class="darka" href="https://github.com/adereth">@adereth</a> (original Dactyl Design), <a class="darka"  href="https://github.com/tshort">@tshort</a> (Dactyl Manuform) and <a class="darka" href="https://github.com/ibnuda">@ibnuda</a> (Dactyl Generator).
 </p>
 <p class="mb-2 text-gray-500 dark:text-gray-400">
-    Do not fear if you see holes in your model preview. Downloading the STL file may give you a complete, hole-free model.
+    Do not fear if you see holes in your model preview or if your keyboard looks holy (sorry). Downloading the STL file may give you a complete, hole-free model.
 </p>
 <p class="mb-2 text-gray-500 dark:text-gray-400">
     You can find this website's source on GitHub. It's licensed under the AGPL.
