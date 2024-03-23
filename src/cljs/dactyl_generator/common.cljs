f04654a7ddb36e477893d6c91165a5594b9f15d8
K-Lohse <151848019+K-Lohse@users.noreply.github.com>
2024-03-23 20:35:33 +0100

    Improvements for Choc (kailh V1) switches:  - Fixed issue with non square key switch holes;  - Reduced left (right) sidewall height of keyholes to fixed 2.2 for the clamps to grip;  - fixed keyheight offset to be level with the surrounding plate.

--- src/cljs/dactyl_generator/common.cljs
+++ src/cljs/dactyl_generator/common.cljs
@@ -270,8 +270,8 @@
                                                      (/ (plate-thickness c) 2)]))
                               :choc (->> (cube (+ (keyswitch-width-c c) 3) holder-thickness (* (plate-thickness c) 0.65))
                                          (translate [0
-                                                     (+ holder-thickness (/ (keyswitch-height c) 2))
-                                                     (* (plate-thickness c) 0.7)]))
+                                                     (+ (/ holder-thickness 2) (/ (keyswitch-height c) 2))
+                                                     ( + (/ (plate-thickness c) 2) (/ (- (plate-thickness c) (* (plate-thickness c) 0.65)) 2))]))
                               (->> (cube (+ (keyswitch-width-c c) mx-margin) holder-thickness (plate-thickness c))
                                    (translate [0
                                                (+ (/ holder-thickness 2) (/ (keyswitch-height c) 2))
@@ -286,10 +286,10 @@
                                                             0
                                                             (- (plate-thickness c)
                                                                (/ alps-notch-height 2))])))
-                              :choc (->> (cube holder-thickness (+ (keyswitch-height c) 3.3) (* (plate-thickness c) 0.65))
+                              :choc (->> (cube holder-thickness (+ (keyswitch-height c) 3.3) 2.2) ;2.2 is the clamp-height of kailh choc V1 switches according to datasheet 
                                          (translate [(+ (/ holder-thickness 2) (/ (keyswitch-width-c c) 2))
                                                      0
-                                                     (* (plate-thickness c) 0.7)]))
+                                                     ( + (/ (plate-thickness c) 2) (/ (- (plate-thickness c) 2.2) 2))]))
                               (->> (cube holder-thickness (+ (keyswitch-height c) mx-margin) (plate-thickness c))
                                    (translate [(+ (/ holder-thickness 2) (/ (keyswitch-width-c c) 2))
                                                0
